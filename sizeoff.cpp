054ca022a5ca320a3713065fef40f6b0585d7729
shreyanshsanu <155836408+shreyanshsanu@users.noreply.github.com>
2024-09-27 12:24:57 +0530

    Add files via upload

---
+++ sizeoff.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a=10;
+	float b=12.2;
+	char c='q';
+	double d=12;
+	printf("size of int %u \n",sizeof(a));
+		printf("size of float  %u \n",sizeof(b));
+			printf("size of char %u \n",sizeof(c));
+				printf("size of double %u \n",sizeof(d));
+	return 0;
+	
+}
