49984568f83302de3b2732865f0c3a09b3e768b4
shreyanshsanu <155836408+shreyanshsanu@users.noreply.github.com>
2024-10-11 22:24:06 +0530

    Update trianglepattern.c

--- trianglepattern.c
+++ trianglepattern.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-#include<conio.h>
-int main()
+int main(){
+
+int n;
+printf("enter the element :");
+scanf("%d",&n);
+for(int i=1;i<=n;i++)
 {
-	int n;
-	printf("enter the num :");
-	scanf("%d",&n);
-    for(int i = 1 ; i<=n ; i++){
-    	for(int s = 1 ; s<=n-i ; s++){
-    		printf(" ");
-		}
-		for(int j = 1 ; j<= 2*i-1 ; j++){
-			if(j == 1 || j == 2*i-1)
-				printf("*");
-			else
-				printf(" -");
-		}
-			printf("\n");
+	for (int s=1;s<=n-i;s++)
+	{
+		printf(" ");
 	}
-	
-return 0 ;	
+	for(int j=1;j<=2*i-1;j++)
+	{
+		if(j==1||j==2*i-1)
+		{
+			printf("*");
+		}
+		else{
+			printf("-");
+		}
+	}
+	printf("\n");
+}
 }
 	
 	
